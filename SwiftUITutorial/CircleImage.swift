1cfee4fc8f12658bf46ee7545877b05cef4053f0
최광익 <cgi4u3@gmail.com>
2019-08-23 21:57:42 +0900

    Tutorial complete

---
+++ SwiftUITutorial/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorial
+//
+//  Created by 최광익 on 2019/08/23.
+//  Copyright © 2019 최광익. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .resizable(resizingMode: .stretch)    // modifier 순서에 따라 적용되지 않을 수 있음(Image에만 적용되는 메소드)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .frame(height: 200)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) { // spacing 적용 가능, content는 trailing closure
+                Text("Turtle Rock")
+                    .font(.title)
+                        // modifier: 기존 view에 변화를 적용하여 다른 view를 리턴
+                                                // 즉, 한 modifier에서 라턴된 view를 받아서 다시 modifier를 적용시키는 메소드 체인임
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding(10.0)
+            
+            Spacer()
+        }
     }
 }
 
